aa0b6e0e524e4757eb60171062f0425a5d974ea8
Lucas Busso <lucasxbusso@gmail.com>
2022-04-27 10:41:18 -0300

    refactoring

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { PrismaClient } from '@prisma/client'
+import Layout from '../layout/Layout'
 
-export default function Home(categorias) {
+export default function Home() {
  
   return (
-    <h1>Next.JS</h1>
+    <Layout>
+      <h1>Inicio</h1>
+    </Layout>
   )
 }
 
-export const getServerSideProps = async () =>{
-  const prisma = new PrismaClient()
-  const categorias = await prisma.categoria.findMany()
-
-  return {
-    props: {
-      categorias,
-    },
-  };
-};
