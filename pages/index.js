0938d5d8ad5e0966124bdfcb4a27da8aca537970
Lucas Busso <lucasxbusso@gmail.com>
2022-04-27 14:57:24 -0300

    instanciando componente producto

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../layout/Layout'
+import Producto from '../components/Producto'
 import useKiosco from '../hooks/useKiosco'
 
 export default function Home() {
@@ -12,6 +13,12 @@ export default function Home() {
       <p className='text-2xl my-10'>
         Elige y personaliza tu pedido
       </p>
+      {categoriaActual?.productos?.map( (producto) => (
+        <Producto   
+          key={producto.id}
+          producto={producto}
+        />
+      ))}
     </Layout>
   )
 }
