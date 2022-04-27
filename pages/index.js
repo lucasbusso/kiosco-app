9219be7a0f7bfd54f676604eaca27112d63dd9f6
Lucas Busso <lucasxbusso@gmail.com>
2022-04-27 12:26:15 -0300

    avance UI en sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../layout/Layout'
+import useKiosco from '../hooks/useKiosco'
 
 export default function Home() {
- 
+  const { categoriaActual } = useKiosco()
+
   return (
-    <Layout>
-      <h1>Inicio</h1>
+    <Layout pagina={`Menú ${categoriaActual?.nombre}`}>
+      <h1 className='text-4xl font-black'>{ categoriaActual?.nombre }</h1>
+      <p className='text-2xl my-10'>
+        Elige y personaliza tu pedido
+      </p>
     </Layout>
   )
 }
