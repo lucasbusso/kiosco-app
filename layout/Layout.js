30fc4a03866a8c6ac516d0cd4231fd467f70a9f8
Lucas Busso <lucasxbusso@gmail.com>
2022-04-27 12:26:38 -0300

    error fix next/head

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,21 +1,23 @@
-import  Head  from "next/document"
-import Sidebar from "../components/Sidebar"
+import  Head  from "next/head"
+import Sidebar from "../components/Sidebar" 
 
 export default function Layout({children, pagina}) {
  
   return(
       <>
-        {/* <Head>
+        <Head>
             <title>Café - {pagina} </title>
             <meta name="description" content="Kiosco Cafetería" />
-        </Head> */}
+        </Head> 
 
         <div className="md:flex">
             <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5">
                 <Sidebar />
             </aside>
             <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
-                {children}
+                <div className="p-10 mt-3">
+                    {children}
+                </div>
             </main>
         </div>
       </>
