2a1529f3c76fa2e6c562826e7f49c414b87676fd
Lucas Busso <lucasxbusso@gmail.com>
2022-04-27 10:42:07 -0300

    refactoring

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { categorias } from "./data/categorias";
+import { categorias } from './data/categorias'
 import { productos } from "./data/productos";
 import { PrismaClient } from "@prisma/client";
 
