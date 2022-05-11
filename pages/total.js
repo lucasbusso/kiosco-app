f20b6fe4f6d26bf3c67b25e8abaf99ead8e0182d
Lucas Busso <lucasxbusso@gmail.com>
2022-05-11 14:01:03 -0300

    componentes dentro del layout con react router

---
+++ pages/total.js
@@ -0,0 +1,13 @@
+import Layout from "../layout/Layout"
+
+export default function Total() {
+    return (
+        <Layout pagina="Resumen">
+            <h1 className="text-4xl font-black">
+                Total del pedido
+            </h1>
+            <p className="text-2xl my-10">
+                Confirma tu pedido a continuación
+            </p>
+        </Layout>
+)}
