d34c1a12d607f961178597902f12da2646d5febf
Lucas Busso <lucasxbusso@gmail.com>
2022-05-15 22:02:20 -0300

    ternario para dibujar la UI de /resumen

--- pages/resumen.js
+++ pages/resumen.js
@@ -1,14 +1,26 @@
 import Layout from "../layout/Layout"
+import useKiosco from "../hooks/useKiosco"
+import ResumenProducto from "../components/ResumenProducto"
 
 export default function Resumen() {
+
+    const { pedido } = useKiosco()
     return (
         <Layout pagina="Resumen">
             <h1 className="text-4xl font-black">
                 Resumen
             </h1>
             <p className="text-2xl my-10">
-                Controla tu pedido 
+                Controlá tu pedido 
             </p>
+            {pedido.length === 0 ? (
+                <p className="text-center text-2xl">Aún no agregaste nada en tu pedido</p>
+            ) : pedido.map(producto => (
+                <ResumenProducto
+                    key={producto.id}
+                    producto={producto}
+                />
+            ))}
         </Layout>
     )
 }
