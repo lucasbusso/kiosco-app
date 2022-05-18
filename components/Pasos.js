3ad48d6488648a9ad86e28a4cf435d74b44bbc8d
Lucas Busso <lucasxbusso@gmail.com>
2022-05-18 20:57:59 -0300

    meti el arreglo al componente

--- components/Pasos.js
+++ components/Pasos.js
@@ -1,13 +1,13 @@
 import { useRouter } from "next/router"
 
-const pasos = [
-    {paso:1, nombre:'Menú', url:"/"},
-    {paso:2, nombre:'Resumen', url:"/resumen"},
-    {paso:3, nombre:'Datos y total', url:"/total"},
-]
-
 const Pasos = () => {
     const router = useRouter()
+    
+    const pasos = [
+        {paso:1, nombre:'Menú', url:"/"},
+        {paso:2, nombre:`Resumen`, url:"/resumen"},
+        {paso:3, nombre:'Datos y total', url:"/total"},
+    ]       
 
     const calcularProgreso = () => {
          let valor
