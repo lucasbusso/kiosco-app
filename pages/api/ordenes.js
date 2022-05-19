f2cdea2d0dffacaf604580b5a9ec10085e31d99b
Lucas Busso <lucasxbusso@gmail.com>
2022-05-18 21:10:15 -0300

    finalizando api

--- pages/api/ordenes.js
+++ pages/api/ordenes.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
                 fecha: req.body.fecha
             }
         })
-        console.log(req.body)
+        
         res.json(orden)
     } 
 }
